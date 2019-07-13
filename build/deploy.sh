12ce2fba29f0d85993f735f45dfa9453b5123e27
Jaehyuk Kim <jaehyuk@xysnc.co>
2019-07-14 08:32:51 +0900

    BLD:배포 환경 설정

--- build/deploy.sh
+++ build/deploy.sh
@@ -1,5 +1,26 @@
 #!/bin/bash
 set -e
-yarn
-npx pm2 install typescript
+CLEAR_LINE='\r\033[K'
+DIRECTORY=server-xsync-bmm-api
+
+cd /home/centos
+
+if ! command -v node > /dev/null; then
+  sudo npm install -g yarn
+fi
+
+if [ ! -d "$DIRECTORY" ]; then
+  git clone git@bitbucket.org:xsync_development/server-xsync-bmm-api.git
+  # Control will enter here if $DIRECTORY doesn't exist.
+fi
+
+cd $DIRECTORY
+
+git pull > /dev/null
+printf "Git pull done ====> \n "
+
+yarn --ignore-engines > /dev/null
+printf "Yarn Done ====> \n "
+
 yarn start
+printf "Server start ====> \n"
