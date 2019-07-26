9439bb15dafb02f10112cbedbbfe0cecd6210455
Jaehyuk Kim <jaehyuk@xysnc.co>
2019-07-26 19:12:35 +0900

    BUG: 자꾸 누락이 되는... 엔티티 왜 그러는거지..

---
+++ src/entity/mysql/entities/MysqlBusinessEventBridge.ts
@@ -0,0 +1,13 @@
+import { Base } from './MysqlBase';
+import { Entity, Column, OneToMany, ManyToMany, OneToOne, JoinColumn } from 'typeorm';
+import { Business } from './MysqlBusiness';
+
+// d이벤트 브릿지
+@Entity()
+export class BusinessEventBridge extends Base {
+    @Column('varchar')
+    eventId: string;
+    @OneToOne(type => Business, business => business.eventBridge, { onDelete: 'CASCADE' })
+    @JoinColumn()
+    business: Business;
+}
