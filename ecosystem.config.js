adad21ccc57252c6cb731e884ebd10865e824a64
Jaehyuk Kim <jaehyuk@xysnc.co>
2019-08-07 12:07:52 +0900

    기록

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,12 +2,13 @@ module.exports = {
     apps: [
         {
             name: 'API',
+            script: './src/app.ts',
             instances: 0,
             autorestart: true,
             watch: false,
             // max_memory_restart: '1.8G',
             env: {
-                ENVIRONMENT: 'production',
+                NODE_ENV: 'production',
             },
         },
     ],
