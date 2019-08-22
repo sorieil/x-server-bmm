06922793a264be642fb6f4379dfbbf2b6e7d33a8
Jaehyuk Kim <jaehyuk@xysnc.co>
2019-08-22 18:08:29 +0900

    예제 추가

---
+++ setTimeout.ts
@@ -0,0 +1,25 @@
+setTimeout(() => {
+    for (let i = 0; i < 10000; i++) {
+        console.log('setTimeout: ', i);
+    }
+}, 0);
+
+for (let i = 0; i < 10000; i++) {
+    console.log('1번: ', i);
+}
+
+for (let i = 0; i < 10000; i++) {
+    console.log('2번: ', i);
+}
+
+for (let i = 0; i < 10000; i++) {
+    console.log('3번: ', i);
+}
+
+for (let i = 0; i < 10000; i++) {
+    console.log('4번: ', i);
+}
+
+for (let i = 0; i < 10000; i++) {
+    console.log('5번: ', i);
+}
